f90b56f7aca754e31b7046829b449d7035d69737
TJ Jefferson <tjefferson4040@gmail.com>
2022-10-19 13:19:04 -0400

    with gitignore

--- src/Firebase/FirebaseSecrets.js
+++ src/Firebase/FirebaseSecrets.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAgwD1jsL-nnq3n7aC3IaveRJGVUVqF4ys",
   authDomain: "jargonlibre.firebaseapp.com",
@@ -16,6 +8,3 @@ const firebaseConfig = {
   measurementId: "G-B5GGQKYC98"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
