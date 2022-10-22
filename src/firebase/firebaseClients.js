c99f50ed947109cb0c9bb3db319a2f93542cdbcd
TJ Jefferson <tjefferson4040@gmail.com>
2022-10-22 17:39:01 -0400

    added firebase clients

---
+++ src/firebase/firebaseClients.js
@@ -0,0 +1,50 @@
+import { getAuth, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getFirestore, doc, collection, setDoc, updateDoc, getDoc } from 'firebase/firestore'
+
+
+class AuthHandler {
+  constructor() {
+    this.uid = undefined
+    this.userCred = undefined
+    this.appAuth = getAuth(window.FIREBASE_APP)
+  }
+
+  async registerUser(email, password) {
+    this.userCred = createUserWithEmailAndPassword(this.appAuth, email, password)
+    this.uid = this.userCred.user.uid;
+    return this.uid
+  }
+
+  async signIn(email, password) {
+    this.userCred = await signInWithEmailAndPassword(this.appAuth, email, password)
+    this.uid = this.userCred.user.uid;
+    return this.uid;
+  }
+
+  getUid() {
+    return this.uid;
+  }
+
+  async signOut() {
+    this.uid = undefined
+    this.userCred = undefined
+    return await signOut(this.appAuth)
+  }
+
+}
+
+
+class DatabaseHandler {
+  constructor() {
+    this.database = getFirestore(window.FIREBASE_APP);
+  }
+
+  async updateSettings(settingsConfig) {
+    await updateDoc(this.database, settingsConfig);
+  }
+
+  async getSettings(userId) {
+    return await getDoc(doc(this.database, "settings", userId))
+  }
+
+}
