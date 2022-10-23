81d262072d4ea12b87ca703b67d6df3d5e434bc3
Deleted User <deleted.user@github.com>
2022-10-23 19:40:55 +0000

    Add PDF.js viewer
    Add pdf host_permission to allow CORS
    Set up PDF.js worker
    Add viewer code

---
+++ src/viewer/utils.ts
@@ -0,0 +1,18 @@
+export function debounce(func: () => void, timeout: number) {
+    let lastCall = 0
+    let timer: number | undefined = undefined
+
+    return () => {
+        let time = Date.now()
+        if (time - lastCall > timeout) {
+            clearTimeout(timer)
+            lastCall = time
+            func()
+        } else {
+            clearTimeout(timer)
+            timer = window.setTimeout(() => {
+                func()
+            }, timeout)
+        }
+    }
+}
