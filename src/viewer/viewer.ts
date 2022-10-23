b2336d7c4bdd6953e20e8bdc7beb6364b8cb64c2
wingdeans <66850754+wingdeans@users.noreply.github.com>
2022-10-23 13:49:09 +0000

    Add mechanism of accessing pdf url from viewer

--- src/viewer/viewer.ts
+++ src/viewer/viewer.ts
@@ -1,1 +1,7 @@
 import { getDocument } from "pdfjs-dist"
+
+const url = new URL(document.location)
+const pdfURL = atob(unescape(url.searchParams.get("url")))
+history.replaceState(null, "", url.origin + url.pathname)
+
+console.log(pdfURL)
