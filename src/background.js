8f4a990b19a175c1be226be0f41fa1b1043d3823
unknown <saganpotenza@gmail.com>
2022-10-24 14:44:49 -0400

    Testing replacement methods in PDFs with inner HTML

--- src/background.js
+++ src/background.js
@@ -5,12 +5,4 @@ browser.browserAction.onClicked.addListener(tab => {
         tab.id,
         {url: browser.runtime.getURL("viewer/viewer.html")}
     )
-	var request = new XMLHttpRequest();
-	request.open("GET", "https://api.dictionaryapi.dev/api/v2/entries/en/cringe", true);
-	request.onreadystatechange = () => {
-		if (request.readyState === 4) {
-			console.log(request.response);
-		}
-	}
-	request.send()
 })
---
+++ src/content/testReplace.js
@@ -0,0 +1,18 @@
+/*const browser = require("webextension-polyfill")
+
+browser.browserAction.onClicked.addListener(tab => {
+    browser.tabs.update(
+        tab.id,
+        {url: browser.runtime.getURL("viewer/viewer.html")}
+    )
+	var url = "https://api.dictionaryapi.dev/api/v2/entries/en/cringe"
+	fetch(url)
+	.then((response) => response.json())
+	.then((data) => console.log(data));
+})
+
+window.addEventListener('load', function () {
+	console.log("loaded")
+	document.getElementById("berd").src = "Berd2.png"
+})*/
+console.log("PDF FOUND")
