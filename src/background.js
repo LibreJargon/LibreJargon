03c6e1849400aebf01b6de7066ef831bc95fce99
TJ Jefferson <tjefferson4040@gmail.com>
2022-11-27 15:17:11 -0500

    popup and pdfParse Integrated

--- src/background.js
+++ src/background.js
@@ -1,11 +1,9 @@
-console.log("HERE")
 const browser = require("webextension-polyfill")
 
 browser.browserAction.onClicked.addListener(tab => {
     const encodedURL = escape(tab.url)
     browser.tabs.update(tab.id, {url: `viewer/viewer.html?url=${encodedURL}`})
+    browser.browserAction.setPopup({
+      popup: "/authentication/auth.html"
+    })
 })
-
-
-
-
