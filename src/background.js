97f5e5b2e4ec3ab0e8fae5572f249176a3fa32f3
TJ Jefferson <tjefferson4040@gmail.com>
2022-12-02 14:37:55 -0500

    New

--- src/background.js
+++ src/background.js
@@ -3,7 +3,4 @@ const browser = require("webextension-polyfill")
 browser.browserAction.onClicked.addListener(tab => {
     const encodedURL = escape(tab.url)
     browser.tabs.update(tab.id, {url: `viewer/viewer.html?url=${encodedURL}`})
-    browser.browserAction.setPopup({
-      popup: "/popup/popup.html"
-    })
 })
