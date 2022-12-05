804870cf80f5dc5d4528f0abe174d1b459ddd40c
TJ Jefferson <tjefferson4040@gmail.com>
2022-12-05 16:08:24 -0500

    auth tests

--- src/popup/tests/popup.test.js
+++ src/popup/tests/popup.test.js
@@ -1,5 +1,7 @@
 import {describe, expect, test} from '@jest/globals';
-import {nonce } from '../utils'
+import { nonce } from '../utils'
+
+
 
 describe('Test Nonce: ', () => {
   test('checks Length is accurate', () => {
