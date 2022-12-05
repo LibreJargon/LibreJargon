5ce8a053a330740e191d7c7c0320cdc372378bfe
TJ Jefferson <tjefferson4040@gmail.com>
2022-12-05 16:16:39 -0500

    added Reading list test

--- src/firebase/tests/firebase.test.js
+++ src/firebase/tests/firebase.test.js
@@ -16,6 +16,25 @@ describe('Test Auth Sign In: ', () => {
   });
 });
 
+describe('Test Reading List: ', () => {
+  test('Pulls reading list and confirms correctness', () => {
+    auth.signIn(testEmail, testPassword)
+    .then((usr) => {
+      db.getReadingList(getAuth().currentUser.uid)
+      .then((rlData) => {
+        expect(rlData).toBe({
+          'R7g6kdUxvR' : {
+            link: 'Link 1',
+            title: 'Title 1'
+          }
+        });
+      })
+  });
+    })
+
+});
+
+
 
 describe('Test Auth Sign In: ', () => {
   test('Checks if user is authenticated', () => {
