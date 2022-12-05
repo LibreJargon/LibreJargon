525da26cf8e3e985e85b6ed4115f4fc93a775cb3
TJ Jefferson <tjefferson4040@gmail.com>
2022-12-05 16:08:24 -0500

    auth tests

--- src/firebase/tests/firebase.test.js
+++ src/firebase/tests/firebase.test.js
@@ -1,8 +1,27 @@
 import {describe, expect, test} from '@jest/globals';
+import { DatabaseHandler, AuthHandler } from '../firebaseClients'
+import { getAuth } from 'firebase/auth';
+
+const db = new DatabaseHandler()
+const auth = new AuthHandler()
+const testEmail = 'test@test.com'
+const testPassword = 'test123'
+
+describe('Test Auth Sign In: ', () => {
+  test('Checks if user is authenticated', () => {
+    auth.signIn(testEmail, testPassword)
+      .then((usr) => {
+        expect(!getAuth().currentUser).toBe(false);
+      })
+  });
+});
 
 
-describe('Test Nonce: ', () => {
-  test('checks Length is accurate', () => {
-    expect(nonce(12).length).toBe(12);
+describe('Test Auth Sign In: ', () => {
+  test('Checks if user is authenticated', () => {
+    auth.signOut()
+      .then((usr) => {
+        expect(getAuth().currentUser).toBe(null);
+      })
   });
 });
