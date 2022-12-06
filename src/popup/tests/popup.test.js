eda30d70e9bf8d0f7e92aa02aa6e85fad8e3089a
TJ Jefferson <tjefferson4040@gmail.com>
2022-12-06 12:36:33 -0500

    first comment

--- src/popup/tests/popup.test.js
+++ src/popup/tests/popup.test.js
@@ -1,8 +1,9 @@
 import {describe, expect, test} from '@jest/globals';
 import { nonce } from '../utils'
+import { getSuggestions } from '../suggestions';
 
 
-
+// First test tests Nonce function for size
 describe('Test Nonce: ', () => {
   test('checks Length is accurate', () => {
     expect(nonce(12).length).toBe(12);
