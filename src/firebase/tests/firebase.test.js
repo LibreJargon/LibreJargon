f30660222a0f4c187cd214bacd4bfe36060ac23d
wingdeans <66850754+wingdeans@users.noreply.github.com>
2022-12-06 12:32:04 -0500

    Add prettier code formatter

--- src/firebase/tests/firebase.test.js
+++ src/firebase/tests/firebase.test.js
@@ -1,70 +1,62 @@
-import {describe, expect, test} from '@jest/globals';
-import { DatabaseHandler, AuthHandler } from '../firebaseClients'
-import { getAuth } from 'firebase/auth';
+import { describe, expect, test } from "@jest/globals";
+import { DatabaseHandler, AuthHandler } from "../firebaseClients";
+import { getAuth } from "firebase/auth";
 
-var db = new DatabaseHandler()
-var auth = new AuthHandler()
-const testEmail = 'test@test.com'
-const testPassword = 'test123'
+var db = new DatabaseHandler();
+var auth = new AuthHandler();
+const testEmail = "test@test.com";
+const testPassword = "test123";
 
-describe('Test Authentication: ', () => {
-  test('Checks if signed in user is authenticated', () => {
-    return auth.signIn(testEmail, testPassword)
-      .then((usr) => {
-        expect(!getAuth().currentUser).toBe(false);
-      })
+describe("Test Authentication: ", () => {
+  test("Checks if signed in user is authenticated", () => {
+    return auth.signIn(testEmail, testPassword).then((usr) => {
+      expect(!getAuth().currentUser).toBe(false);
+    });
   });
-  
-  test('Checks if signed out user is authenticated', () => {
-    return auth.signOut()
-      .then((usr) => {
-        expect(getAuth().currentUser).toBe(null);
-      })
+
+  test("Checks if signed out user is authenticated", () => {
+    return auth.signOut().then((usr) => {
+      expect(getAuth().currentUser).toBe(null);
+    });
   });
 });
 
-describe('Test Each Part of Database: ', () => {
-  test('Pulls reading list and confirms correctness', () => {
-    return auth.signIn(testEmail, testPassword)
-    .then((usr) => {
-      return db.getReadingList(getAuth().currentUser.uid)
-      .then((rlData) => {
+describe("Test Each Part of Database: ", () => {
+  test("Pulls reading list and confirms correctness", () => {
+    return auth.signIn(testEmail, testPassword).then((usr) => {
+      return db.getReadingList(getAuth().currentUser.uid).then((rlData) => {
         expect(rlData).toStrictEqual({
-          'R7g6kdUxvR' : {
-            title: 'Title 1',
-            link: 'Link 1'
-          }
+          R7g6kdUxvR: {
+            title: "Title 1",
+            link: "Link 1",
+          },
         });
-      })
+      });
     });
-  })
-	
-  test('Pulls jargon list and confirms correctness', () => {
-    return auth.signIn(testEmail, testPassword)
-    .then((usr) => {
-      db.getJargon(getAuth().currentUser.uid)
-      .then((jargonData) => {
+  });
+
+  test("Pulls jargon list and confirms correctness", () => {
+    return auth.signIn(testEmail, testPassword).then((usr) => {
+      db.getJargon(getAuth().currentUser.uid).then((jargonData) => {
         expect(jargonData).toStrictEqual({
-          '3tCsFDwGkT' : {
-            word: 'algorithm'
-          }
+          "3tCsFDwGkT": {
+            word: "algorithm",
+          },
         });
-      })
+      });
     });
-  })
-  
-  test('Pulls settings list and confirms correctness', () => {
-    return auth.signIn(testEmail, testPassword)
-    .then((usr) => {
-      db.getSettings(getAuth().currentUser.uid)
-      .then((settingData) => {
+  });
+
+  test("Pulls settings list and confirms correctness", () => {
+    return auth.signIn(testEmail, testPassword).then((usr) => {
+      db.getSettings(getAuth().currentUser.uid).then((settingData) => {
         expect(settingData).toStrictEqual({
-          'a47qPF9q0Z' : {
+          a47qPF9q0Z: {
             name: "Replace All Instances of Jargon",
-			value: false
-          }
+            value: false,
+          },
         });
-      })
+      });
     });
-  })
+  });
 });
--- src/popup/suggestions.js
+++ src/popup/suggestions.js
@@ -1,55 +1,59 @@
-import { nonce } from "./utils"
+import { nonce } from "./utils";
 
 // const arxiv = require('arxiv-api');
 
-
 function renderSuggestions() {
-  arxiv.search({
-    searchQueryParams: [
+  arxiv
+    .search({
+      searchQueryParams: [
         {
-            include: [{name: 'Climate'}, {name: 'Deep learning'}],
-        }
-    ],
-    start: 0,
-    maxResults: 5,
-}).then((res) => {
-  for(var paper of res) {
-    renderRLItem(paper.title, paper.id, nonce(10))
-  }
-}).catch((e) => {
-  console.log(e)
-})
+          include: [{ name: "Climate" }, { name: "Deep learning" }],
+        },
+      ],
+      start: 0,
+      maxResults: 5,
+    })
+    .then((res) => {
+      for (var paper of res) {
+        renderRLItem(paper.title, paper.id, nonce(10));
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
 
 function renderRLItem(title, link, id) {
   var ul = document.getElementById("suggestion-list");
   var li = document.createElement("li");
-  li.className = "rl-li"
-  var addBtn = document.createElement("button")
+  li.className = "rl-li";
+  var addBtn = document.createElement("button");
   addBtn.className = "icons";
-  addBtn.textContent = "+"
+  addBtn.textContent = "+";
 
-  addBtn.addEventListener('click', () => {
+  addBtn.addEventListener("click", () => {
     const liToDel = document.getElementById(id);
     liToDel.style.visibility = "hidden";
     liToDel.style.height = "0px";
-    ul.removeAttribute(liToDel)
-    self.dbHandler.addToReadingList({
-      title: title,
-      link:link
-    },self.user.uid, id)
-
+    ul.removeAttribute(liToDel);
+    self.dbHandler.addToReadingList(
+      {
+        title: title,
+        link: link,
+      },
+      self.user.uid,
+      id
+    );
   });
 
   var titlep = document.createElement("a");
-  titlep.appendChild(document.createTextNode(title))
+  titlep.appendChild(document.createTextNode(title));
   titlep.title = title;
   titlep.href = link;
   li.id = id;
-  li.appendChild(addBtn)
-  li.appendChild(titlep)
+  li.appendChild(addBtn);
+  li.appendChild(titlep);
   ul.appendChild(li);
 }
 
-
-export { renderSuggestions }
+export { renderSuggestions };
