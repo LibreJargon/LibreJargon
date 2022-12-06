d499c00536c53f546fefdf08e9299ba40ce24cf9
TJ Jefferson <tjefferson4040@gmail.com>
2022-12-06 15:14:16 -0500

    code smell no mo

--- src/popup/utils.js
+++ src/popup/utils.js
@@ -1,8 +1,8 @@
 function nonce(length) {
-  var text = "";
-  var possible =
+  let text = "";
+  let possible =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i = 0; i < length; i++) {
+  for (const i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
