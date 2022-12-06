4e9608deb6db4ad7d1877867f014483efb6178f1
Sagan Potenza <64374401+spotenza2016@users.noreply.github.com>
2022-12-06 14:47:40 -0500

    Fix sonarcloud (#52)
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Update config.yml
    
    * Update .gitignore
    
    * Add files via upload

---
+++ src/firebase/firebaseSecrets.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAgwD1jsL-nnq3n7aC3IaveRJGVUVqF4ys",
+  authDomain: "jargonlibre.firebaseapp.com",
+  projectId: "jargonlibre",
+  storageBucket: "jargonlibre.appspot.com",
+  messagingSenderId: "1034819114150",
+  appId: "1:1034819114150:web:61aed4acc72e7d02f55067",
+  measurementId: "G-B5GGQKYC98"
+};
